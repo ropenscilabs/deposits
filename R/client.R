f0b0fb6226eb627f776bcb222beaa4eb8c6dc22b
mpadge <mark.padgham@email.com>
2021-10-13 12:31:29 +0200

    lint

--- R/client.R
+++ R/client.R
@@ -10,8 +10,8 @@
 #' # make a client
 #' token <- Sys.getenv("ZENODO_TOKEN")
 #' cli <- depositsClient$new(
-#'   url = "https://zenodo.org/api",
-#'   headers = list(Authorization = paste0("Bearer ", token))
+#'     url = "https://zenodo.org/api",
+#'     headers = list(Authorization = paste0("Bearer ", token))
 #' )
 #'
 #' # methods
@@ -19,48 +19,48 @@
 #' cli$ping()
 #'
 #' }
-depositsClient <- R6::R6Class(
-  "depositsClient",
-  portable = TRUE,
-  cloneable = FALSE,
+depositsClient <- R6::R6Class( # nolint (not snake_case)
+    "depositsClient",
+    portable = TRUE,
+    cloneable = FALSE,
 
-  private = list(
-  ), # end private list
+    private = list(
+    ), # end private list
 
-  public = list(
-    #' @field url (character) list of fragments
-    url = NULL,
-    #' @field headers list of named headers
-    headers = NULL,
-    #' @field schema holds schema
-    schema = NULL,
-    #' @field result holds result from http request
-    result = NULL,
+    public = list(
+        #' @field url (character) list of fragments
+        url = NULL,
+        #' @field headers list of named headers
+        headers = NULL,
+        #' @field schema holds schema
+        schema = NULL,
+        #' @field result holds result from http request
+        result = NULL,
 
-    #' @description Create a new `depositsClient` object
-    #' @param url (character) URL for the deposits schema
-    #' @param headers Any acceptable headers, a named list. See examples
-    #' @return A new `depositsClient` object
-    initialize = function(url, headers) {
-      if (!missing(url)) self$url <- url
-      if (!missing(headers)) self$headers <- headers
-    },
+        #' @description Create a new `depositsClient` object
+        #' @param name (character) of the deposits server
+        #' @param headers Any acceptable headers, a named list. See examples
+        #' @return A new `depositsClient` object
+        initialize = function(url, headers) {
+            if (!missing(url)) self$url <- url
+            if (!missing(headers)) self$headers <- headers
+        },
 
-    #' @description print method for the `depositsClient` class
-    #' @param x self
-    #' @param ... ignored
-    print = function(x, ...) {
-      cat('<deposits client>', sep = "\n")
-      cat(paste0('  url: ', self$url), sep = "\n")
-    },
+        #' @description print method for the `depositsClient` class
+        #' @param x self
+        #' @param ... ignored
+        print = function(x, ...) {
+            cat("<deposits client>", sep = "\n")
+            cat(paste0("  url: ", self$url), sep = "\n")
+        },
 
-    #' @description ping a deposits server
-    #' @param ... curl options passed on to [crul::verb-HEAD]
-    #' @return `TRUE` if successful response, `FALSE` otherwise
-    ping = function(...) {
-      res <- deposits_HEAD(self$url, self$headers, ...)
-      res$success()
-    }
+        #' @description ping a deposits server
+        #' @param ... curl options passed on to [crul::verb-HEAD]
+        #' @return `TRUE` if successful response, `FALSE` otherwise
+        ping = function(...) {
+            res <- deposits_HEAD(self$url, self$headers, ...)
+            res$success()
+        }
 
-  ) # end public list
+    ) # end public list
 )
--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
 zenodo_base <- function() "https://zenodo.org/api"
 
-deposits_HEAD <- function(url, headers, ...){
-  con <- crul::HttpClient$new(url, headers = headers, opts = list(...))
-  res <- con$head()
-  res$raise_for_status()
-  res
+deposits_HEAD <- function(url, headers, ...) { # nolint (not snake_case)
+    con <- crul::HttpClient$new(url, headers = headers, opts = list(...))
+    res <- con$head()
+    res$raise_for_status()
+    res
 }
