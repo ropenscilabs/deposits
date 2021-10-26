6a87309fabec8cf142d0337250f1f34a97f183d2
mpadge <mark.padgham@email.com>
2021-10-26 13:22:03 +0200

    fix test

--- tests/testthat/test-services.R
+++ tests/testthat/test-services.R
@@ -7,6 +7,6 @@ test_that("services", {
     expect_s3_class (s, "data.frame")
     expect_equal (ncol (s), 3L)
     expect_equal (nrow (s), 2L)
-    expect_identical (names (s), c ("name", "docs", "api-base-url"))
+    expect_identical (names (s), c ("name", "docs", "api_base_url"))
     expect_identical (s$name, c ("zenodo", "figshare"))
 })
