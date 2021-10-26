00a2885b7b73beee3c4b418c58794aa25305d69e
mpadge <mark.padgham@email.com>
2021-10-26 11:54:07 +0200

    add tests

---
+++ tests/testthat/test-services.R
@@ -0,0 +1,12 @@
+
+test_that("services", {
+
+    expect_silent (
+        s <- deposits_services ()
+        )
+    expect_s3_class (s, "data.frame")
+    expect_equal (ncol (s), 3L)
+    expect_equal (nrow (s), 2L)
+    expect_identical (names (s), c ("name", "docs", "api-base-url"))
+    expect_identical (s$name, c ("zenodo", "figshare"))
+})
