70d892318a52e8f0adf949e410fe86fd4bcc43c4
mpadge <mark.padgham@email.com>
2022-02-23 13:30:34 +1100

    add deposits_meta_to_dcmi fn

--- R/metadata.R
+++ R/metadata.R
@@ -19,7 +19,7 @@
 #' filename <- tempfile (fileext = ".json")
 #' deposits_metadata_template (filename)
 #' # then edit that file to complete metadata
-#' @family misc
+#' @family meta
 #' @export
 deposits_metadata_template <- function (filename = NULL) {
 
@@ -27,14 +27,13 @@ deposits_metadata_template <- function (filename = NULL) {
         stop ("filename [", filename, "] already exists; please delete before ",
               "calling this function.")
     }
-        
+
     # Get DCMI metadata fields from atom4R function names:
     fields <- ls (envir = asNamespace ("atom4R"), pattern = "^DC")
     not_meta <- c ("DCElement", "DCEntry", "DCMIVocabulary")
     fields <- fields [which (!fields %in% not_meta)]
 
     fields <- gsub ("^DC", "", fields)
-    substr (fields, 1, 1) <- tolower (substr (fields, 1, 1))
     flist <- as.list (rep ("", length (fields)))
     names (flist) <- fields
 
@@ -47,4 +46,69 @@ deposits_metadata_template <- function (filename = NULL) {
         ), error = function (e) e)
 
     invisible (!methods::is (res, "error"))
+}
+
+#' Read a metadata `yaml` file and convert to \pkg{atom4R} DCMI object.
+#'
+#' Metadata templates can be generated with \link{deposits_metadata_template},
+#' and values manually entered. This functions loads the completed template and
+#' converts it to a `DCEntry` object from the \pkg{atom4R} package.
+#'
+#' @param filename Name of completed metadata file generated from
+#' \link{deposits_metadata_template}.
+#' @param id A unique identifier for the deposit, as a single character string.
+#'
+#' @examples
+#' filename <- tempfile (fileext = ".json")
+#' deposits_metadata_template (filename)
+#' m <- readLines (filename)
+#' m [grep ("Title", m)] <- "  \"Title\": \"New Title\""
+#' m [grep ("Type", m)] <- "  \"Type\": \"Software\""
+#' m [grep ("Description", m)] <- "  \"Description\": \"Description of software\""
+#' # Values can be entered in JSON format:
+#' m [grep ("TableOfContents", m)] <-
+#' "  \"TableOfContents\": {\"one\": \"First\", \"two\": \"Second\"}"
+#' dc <- deposits_meta_to_dcmi (filename, id = "my-id")
+#' @family meta
+#' @export
+deposits_meta_to_dcmi <- function (filename = NULL, id = "my-id") {
+
+    if (is.null (filename)) {
+        stop ("filename must be specified.")
+    }
+    if (!file.exists (filename)) {
+        stop ("filename [", filename, "] does not exist.")
+    }
+
+
+    meta <- readLines (filename)
+    check <- jsonlite::validate (meta)
+    if (!check) {
+        stop ("json is not valid.")
+    }
+
+    if (!"atom4R" %in% loadedNamespaces ()) {
+        # https://github.com/eblondel/atom4R/issues/9
+        library ("atom4R")
+    }
+
+    meta <- jsonlite::read_json (filename)
+    meta <- meta [which (nchar (meta) > 0L)]
+    #for (i in seq_along (meta)) {
+    #    meta [[i]] <- jsonlite::toJSON (meta [[i]], auto_unbox = TRUE)
+    #}
+
+    dcmi <- atom4R::DCEntry$new ()
+
+    for (n in names (meta)) {
+        dc_fn <- paste0 ("addDC", n)
+        meta_n <- meta [[n]]
+        for (m in meta_n) {
+            do.call (dcmi [[dc_fn]], list (m))
+        }
+    }
+
+    check <- dcmi$validate ()
+
+    return (dcmi)
 }
