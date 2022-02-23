842a0a9673c9b8bba9e2ba21cdd3ecd232dc9ccc
mpadge <mark.padgham@email.com>
2022-02-23 13:41:38 +1100

    test metadata to DCEntry

--- tests/testthat/test-metadata.R
+++ tests/testthat/test-metadata.R
@@ -15,3 +15,31 @@ test_that("metadata template", {
         out <- deposits_metadata_template (filename),
         "already exists; please delete before calling this function.")
 })
+
+test_that("metadata to DCEntry", {
+
+    filename <- tempfile (fileext = ".json")
+    deposits_metadata_template (filename)
+
+    m <- readLines (filename)
+    m [grep ("Title", m)] <- "  \"Title\": \"New Title\","
+    m [grep ("Type", m)] <- "  \"Type\": \"Software\","
+    m [grep ("Description", m)] <- "  \"Description\": \"Description of software\","
+    m [grep ("TableOfContents", m)] <-
+        "  \"TableOfContents\": {\"one\": \"First\", \"two\": \"Second\"},"
+
+    expect_true (jsonlite::validate (m))
+    writeLines (m, filename)
+
+    expect_output (
+        dc <- deposits_meta_to_dcmi (filename, id = "my-id")
+        )
+    expect_s3_class (dc, "DCEntry")
+
+    expect_identical (dc$title [[1]]$value, "New Title")
+    expect_identical (dc$type [[1]]$value, "Software")
+    expect_identical (dc$description [[1]]$value, "Description of software")
+    expect_length (dc$tableOfContents, 2L)
+    expect_identical (dc$tableOfContents [[1]]$value, "First")
+    expect_identical (dc$tableOfContents [[2]]$value, "Second")
+})
