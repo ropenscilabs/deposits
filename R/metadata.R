3a3e52b5d824d7b372f8baea588861141961ef6f
mpadge <mark.padgham@email.com>
2022-02-24 14:56:48 +1100

    add validate_zenodo_terms fn

--- R/metadata.R
+++ R/metadata.R
@@ -183,13 +183,118 @@ construct_data_list <- function (metadata, term_map) {
         } else if (names (meta_values$creators) == "name") {
             meta_values$creators <- list (meta_values$creators)
         }
+        if ("upload_type" %in% names (meta_values)) {
+            meta_values$upload_type <- tolower (meta_values$upload_type)
+        }
 
         values$metadata <- meta_values
 
         if (!"created" %in% names (values)) {
             values <- c ("created" = paste0 (Sys.Date ()), values)
         }
+
     }
 
     return (values)
+}
+
+validate_terms <- function (terms, deposit = "zenodo") {
+
+    deposit <- match.arg (deposit, c ("figshare", "zenodo"))
+
+    if (deposit == "zenodo") {
+        res <- validate_zenodo_terms (terms)
+    } else if (deposit == "figshare") {
+        res <- validate_figshare_terms (terms)
+    }
+
+    return (res)
+}
+
+validate_zenodo_terms <- function (terms) {
+
+    out <- NULL
+
+    meta <- terms$metadata
+    terms$metadata <- NULL
+
+    f <- system.file (file.path ("extdata", "zenodoTerms.csv"),
+                      package = "deposits")
+    zen_terms <- utils::read.csv (f)
+    for (i in seq (ncol (zen_terms))) {
+        zen_terms [, i] <- gsub ("^\\s+|\\s+$", "", zen_terms [, i])
+    }
+    zen_terms$metadata <- as.logical (zen_terms$metadata)
+    zen_terms <- zen_terms [which (nzchar (zen_terms$vocabulary)), ]
+
+    zen_meta_terms <- zen_terms [which (zen_terms$metadata), ]
+    zen_terms <- zen_terms [which (!zen_terms$metadata), ]
+
+    index <- which (zen_meta_terms$term %in% names (meta))
+    zen_meta_terms <- zen_meta_terms [index, ]
+    zen_terms <- zen_terms [which (zen_terms$term %in% names (terms)), ]
+
+    # check terms
+    for (i in seq (nrow (zen_terms))) {
+        values <- strsplit (zen_terms$vocabulary [i], "\\|") [[i]]
+        term_i <- terms [[zen_terms$term [i]]]
+        if (!term_i %in% values) {
+            out <- c (out,
+                      paste0 ("Data [",
+                              zen_terms$term [i],
+                              " = '",
+                              term_i,
+                              "'] not in required vocabulary of [",
+                              zen_terms$vocabulary [i],
+                              "]"))
+        }
+    }
+
+    # check meta_terms
+    for (i in seq (nrow (zen_meta_terms))) {
+
+        if (grepl ("\\.csv$", zen_meta_terms$vocabulary [i])) {
+            f <- system.file (file.path ("extdata",
+                                         zen_meta_terms$vocabulary [i]),
+                              package = "deposits")
+            voc <- utils::read.csv (f)
+            term_i <- meta [[zen_meta_terms$term [i]]]
+            if (zen_meta_terms$format [i] == "array") {
+                if (!is.list (term_i)) {
+                    out <- c (out,
+                              paste0 ("Metadata [",
+                                      zen_meta_terms$term [i],
+                                      "] must be an array"))
+                }
+            }
+        } else if (zen_meta_terms$format [i] == "array") {
+            values <- strsplit (zen_meta_terms$vocabulary [i], "\\|") [[1]]
+            term_i <- unique (unlist (lapply (meta$creators, names)))
+            if (!all (term_i %in% values)) {
+                out <- c (out,
+                          paste0 ("Metadata [",
+                                  zen_meta_terms$term [i],
+                                  " = '",
+                                  term_i,
+                                  "'] not in required vocabulary of [",
+                                  zen_meta_terms$vocabulary [i],
+                                  "]"))
+            }
+        } else {
+            values <- strsplit (zen_meta_terms$vocabulary [i], "\\|") [[1]]
+            term_i <- meta [[zen_meta_terms$term [i]]]
+            if (!term_i %in% values) {
+                out <- c (out,
+                          paste0 ("Metadata [",
+                                  zen_meta_terms$term [i],
+                                  " = '",
+                                  term_i,
+                                  "'] not in required vocabulary of [",
+                                  zen_meta_terms$vocabulary [i],
+                                  "]"))
+            }
+        }
+    }
+
+    return (out)
 }
