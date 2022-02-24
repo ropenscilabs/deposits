9405d04826d7129dd5f53c5c572cf9eed3b15544
mpadge <mark.padgham@email.com>
2022-02-24 13:51:47 +1100

    add zenodo_licenses extdata

---
+++ data-raw/get-zenodo-licenses.R
@@ -0,0 +1,11 @@
+token <- Sys.getenv ("ZENODO_TOKEN")
+url <- "https://zenodo.org/api/licenses?size=1000"
+headers <- list (Authorization = paste0 ("Bearer ", token))
+con <- crul::HttpClient$new (url, headers = headers)
+res <- con$get ()
+lic <- jsonlite::fromJSON (res$parse (encoding = "UTF-8"))
+lic <- lic$hits$hits$metadata
+lic <- lic [, c ("id", "title", "url")]
+
+path <- file.path (here::here (), "inst", "extdata", "zenodo_licenses.csv")
+write.csv (lic, file = path, row.names = FALSE, fileEncoding = "UTF-8")
