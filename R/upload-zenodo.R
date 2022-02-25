8ec766c8bbff5e25fbbe8815a71c1a430cf21228
mpadge <mark.padgham@email.com>
2022-02-25 11:14:32 +1100

    finish upload-zenodo method; closes #4

--- R/upload-zenodo.R
+++ R/upload-zenodo.R
@@ -1,11 +1,15 @@
-upload_zenodo_file <- function (id, url, headers, path) {
+#' Upload a file to a nominated zenodo article
+#'
+#' @param deposit_id 'id' parameter passed to the deposits
+#' client.
+#' @param url The generic endpoint for zenodo articles.
+#' @param headers Generic header including the OAuth token.
+#' @param path Path to local file to be uploaded.
+#' @noRd
+upload_zenodo_file <- function (deposit_id, url, headers, path) {
 
-    con <- crul::HttpClient$new (url, headers = headers)
-    res <- con$get ()
-    res$raise_for_status ()
-    res <- jsonlite::fromJSON (res)
+    bucket_link <- get_zenodo_bucket_link (depost_id, url, headers)
 
-    bucket_link <- res$links$bucket
     filename <- basename (path)
     file_url <- paste0 (bucket_link, "/", filename)
     headers <- c (headers, "Content-Type" = "application/octet-stream")
@@ -15,4 +19,16 @@ upload_zenodo_file <- function (id, url, headers, path) {
     res$raise_for_status ()
 
     return (res)
+}
+
+get_zenodo_bucket_link <- function (depost_id, url, headers) {
+
+    url <- sprintf ("%s/%s", url, deposit_id)
+
+    con <- crul::HttpClient$new (url, headers = headers)
+    res <- con$get ()
+    res$raise_for_status ()
+    x <- jsonlite::fromJSON (res$parse (encoding = "UTF-8"))
+
+    return (x$links$bucket)
 }
