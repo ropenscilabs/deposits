f4823507fa2257be5a765967881d50a35194098d
mpadge <mark.padgham@email.com>
2022-05-16 10:29:26 +0200

    add description of validate_terms fn [ci skip]

--- R/metadata-validate.R
+++ R/metadata-validate.R
@@ -1,3 +1,9 @@
+#' validate metadata terms
+#'
+#' @param terms A list of metadata terms returned from `construct_date_list()`.
+#' @param deposit Name of deposits service.
+#' @return `NULL` if all terms are valid, otherwise a vector of any invalid terms.
+#' @noRd
 validate_terms <- function (terms, deposit = "zenodo") {
 
     deposit <- match.arg (deposit, c ("figshare", "zenodo"))
