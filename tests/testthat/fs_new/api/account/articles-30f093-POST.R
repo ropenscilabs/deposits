5f440b68d15098309dae039daaeb5a56409693db
mpadge <mark.padgham@email.com>
2022-05-18 13:17:04 +0200

    re-generate mock test results

--- tests/testthat/fs_new/api/account/articles-30f093-POST.R
+++ tests/testthat/fs_new/api/account/articles-30f093-POST.R
@@ -1,8 +1,8 @@
 structure(list(method = "POST", url = "api/account/articles", 
-    status_code = 201L, headers = structure(list(date = "Tue, 17 May 2022 13:03:07 GMT", 
+    status_code = 201L, headers = structure(list(date = "Wed, 18 May 2022 11:03:33 GMT", 
         `content-type` = "application/json", `content-length` = "108", 
-        location = "api/articles19779811", server = "nginx", 
+        location = "api/articles19786306", server = "nginx", 
         `access-control-allow-methods` = "GET, POST, PUT, DELETE, OPTIONS", 
         `access-control-allow-headers` = "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization", 
         `access-control-max-age` = "3600"), class = "httr2_headers"), 
-    body = charToRaw("{\"entity_id\": 19779811, \"location\": \"api/articles19779811\", \"warnings\": []}")), class = "httr2_response")
+    body = charToRaw("{\"entity_id\": 19786306, \"location\": \"api/articles19786306\", \"warnings\": []}")), class = "httr2_response")
--- tests/testthat/fs_up/api/articles19786306/files-4bd53e-POST.R
+++ tests/testthat/fs_up/api/articles19786306/files-4bd53e-POST.R
@@ -1,8 +1,8 @@
-structure(list(method = "POST", url = "api/articles19779811/files", 
-    status_code = 201L, headers = structure(list(date = "Tue, 17 May 2022 13:03:07 GMT", 
+structure(list(method = "POST", url = "api/articles19786306/files", 
+    status_code = 201L, headers = structure(list(date = "Wed, 18 May 2022 11:03:34 GMT", 
         `content-type` = "application/json", `content-length` = "84", 
-        location = "api/articles19779811/files/35141656", server = "nginx", 
+        location = "api/articles19786306/files/35155804", server = "nginx", 
         `access-control-allow-methods` = "GET, POST, PUT, DELETE, OPTIONS", 
         `access-control-allow-headers` = "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization", 
         `access-control-max-age` = "3600"), class = "httr2_headers"), 
-    body = charToRaw("{\"location\": \"api/articles19779811/files/35141656\"}")), class = "httr2_response")
+    body = charToRaw("{\"location\": \"api/articles19786306/files/35155804\"}")), class = "httr2_response")
