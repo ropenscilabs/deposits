a06390ac7b86af28c7d3954ff2e052009f33b3fc
mpadge <mark.padgham@email.com>
2022-05-19 12:47:31 +0200

    restructure metadata_validate into separate fns

--- R/metadata-validate.R
+++ R/metadata-validate.R
@@ -98,91 +98,121 @@ check_zenodo_meta_terms <- function (zen_meta_terms, meta) {
     for (i in seq (nrow (zen_meta_terms))) {
 
         if (grepl ("\\.csv$", zen_meta_terms$vocabulary [i])) {
-            f <- system.file (file.path (
-                "extdata",
-                zen_meta_terms$vocabulary [i]
-            ),
-            package = "deposits"
-            )
-            voc <- utils::read.csv (f)
-            if (zen_meta_terms$term [i] == "license") {
-                voc <- c ("cc-zero", "cc-by", voc$id)
-            }
-            term_i <- meta [[zen_meta_terms$term [i]]]
-            if (zen_meta_terms$format [i] == "array") {
-                if (!is.list (term_i)) {
-                    out <- c (
-                        out,
-                        paste0 (
-                            "Metadata [",
-                            zen_meta_terms$term [i],
-                            "] must be an array"
-                        )
-                    )
-                }
-            } else if (!term_i %in% voc) {
-                out <- c (
-                    out,
-                    paste0 (
-                        "Metadata [",
-                        zen_meta_terms$term [i],
-                        " = '",
-                        term_i,
-                        "'] not in required vocabulary."
-                    )
-                )
-            }
-        } else if (nzchar (zen_meta_terms$vocabulary [i])) {
-            values <- strsplit (zen_meta_terms$vocabulary [i], "\\|") [[1]]
-            term_i <- meta [[zen_meta_terms$term [i]]]
-            if (zen_meta_terms$format [i] == "array") {
 
-                term_names <- unique (unlist (lapply (term_i, names)))
-                if (!all (term_names %in% values)) {
-                    out <- c (
-                        out,
-                        paste0 (
-                            "Metadata [",
-                            zen_meta_terms$term [i],
-                            "] must be an array/list ",
-                            "with names in [",
-                            paste0 (values, collapse = ", "),
-                            "]"
-                        )
-                    )
-                }
-            } else if (!term_i %in% values) {
-                out <- c (
-                    out,
-                    paste0 (
-                        "Metadata [",
-                        zen_meta_terms$term [i],
-                        " = '",
-                        term_i,
-                        "'] not in required vocabulary of [",
-                        zen_meta_terms$vocabulary [i],
-                        "]"
-                    )
-                )
-            }
+            out <- c (out, check_zen_meta_from_file (zen_meta_terms, meta, i))
+
+        } else if (nzchar (zen_meta_terms$vocabulary [i])) {
+
+            out <- c (out, check_zen_meta_from_vocab (zen_meta_terms, meta, i))
+
         } else if (zen_meta_terms$format [i] == "array") {
-            term_i <- meta [[zen_meta_terms$term [i]]]
-            if (!is.list (term_i)) {
-                out <- c (
-                    out,
-                    paste0 (
-                        "Metadata [",
-                        zen_meta_terms$term [i],
-                        "] must be an array/list object"
-                    )
-                )
-            }
+
+            out <- c (out, check_zen_meta_array (zen_meta_terms, i))
         }
     }
 
     return (out)
 }
 
+#' Check one zenodo metadata term against vocabulary file
+#' @noRd
+check_zen_meta_from_file <- function (zen_meta_terms, meta, i) {
+
+    out <- NULL
+
+    f <- system.file (file.path (
+        "extdata",
+        zen_meta_terms$vocabulary [i]
+    ),
+    package = "deposits"
+    )
+    voc <- utils::read.csv (f)
+    if (zen_meta_terms$term [i] == "license") {
+        voc <- c ("cc-zero", "cc-by", voc$id)
+    }
+    term_i <- meta [[zen_meta_terms$term [i]]]
+
+    if (zen_meta_terms$format [i] == "array") {
+
+        if (!is.list (term_i)) {
+            out <- paste0 (
+                "Metadata [",
+                zen_meta_terms$term [i],
+                "] must be an array"
+            )
+        }
+
+    } else if (!term_i %in% voc) {
+
+        out <- paste0 (
+            "Metadata [",
+            zen_meta_terms$term [i],
+            " = '",
+            term_i,
+            "'] not in required vocabulary."
+        )
+    }
+
+    return (out)
+}
+
+#' Check one zenodo metadata term against vocabulary entry
+#' @noRd
+check_zen_meta_from_vocab <- function (zen_meta_terms, meta, i) {
+
+    out <- NULL
+
+    values <- strsplit (zen_meta_terms$vocabulary [i], "\\|") [[1]]
+    term_i <- meta [[zen_meta_terms$term [i]]]
+
+    if (zen_meta_terms$format [i] == "array") {
+
+        term_names <- unique (unlist (lapply (term_i, names)))
+        if (!all (term_names %in% values)) {
+            out <- paste0 (
+                "Metadata [",
+                zen_meta_terms$term [i],
+                "] must be an array/list ",
+                "with names in [",
+                paste0 (values, collapse = ", "),
+                "]"
+            )
+        }
+
+    } else if (!term_i %in% values) {
+
+        out <- paste0 (
+            "Metadata [",
+            zen_meta_terms$term [i],
+            " = '",
+            term_i,
+            "'] not in required vocabulary of [",
+            zen_meta_terms$vocabulary [i],
+            "]"
+        )
+    }
+
+    return (out)
+}
+
+#' Check one zenodo metadata array term
+#' @noRd
+check_zen_meta_array <- function (zen_meta_terms, i) {
+
+    out <- NULL
+
+    term_i <- meta [[zen_meta_terms$term [i]]]
+
+    if (!is.list (term_i)) {
+        out <- paste0 (
+            "Metadata [",
+            zen_meta_terms$term [i],
+            "] must be an array/list object"
+        )
+    }
+
+    return (out)
+}
 
 validate_figshare_terms <- function (terms) {
 
