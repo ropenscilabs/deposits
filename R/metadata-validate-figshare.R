0b9285e5e5323bf047f5e4657fe5ac5d00a24585
mpadge <mark.padgham@email.com>
2022-05-19 13:03:45 +0200

    restructure metadata-validate-figshare.R

--- R/metadata-validate-figshare.R
+++ R/metadata-validate-figshare.R
@@ -16,70 +16,104 @@ validate_figshare_terms <- function (terms) {
     for (i in seq (nrow (fs_terms))) {
 
         term_i <- terms [[fs_terms$term [i]]]
+
         if (fs_terms$format [i] == "integer") {
-            if (is.na (suppressWarnings (as.integer (term_i)))) {
-                out <- c (
-                    out,
-                    paste0 (
-                        "Data [",
-                        fs_terms$term [i],
-                        "] is not coercible to integer."
-                    )
-                )
-            }
+
+            out <- c (out, check_fs_meta_integer (fs_terms, i, term_i))
+
         } else if (grepl ("^(array|list)", fs_terms$format [i])) {
-            if (!is.list (term_i)) {
-                out <- c (
-                    out,
-                    paste0 (
-                        "Data [",
-                        fs_terms$term [i],
-                        "] must have format [",
-                        fs_terms$format [i],
-                        "]"
-                    )
-                )
-            } else if (nzchar (fs_terms$vocabulary [i])) {
-                voc <- strsplit (fs_terms$vocabulary [i], "\\|") [[1]]
-                term_names <- c (
-                    names (term_i),
-                    unlist (lapply (term_i, names))
-                )
-                if (!all (term_names %in% voc)) {
-                    out <- c (
-                        out,
-                        paste0 (
-                            "Data [",
-                            fs_terms$term [i],
-                            " = '",
-                            term_i,
-                            "' must follow fixed vocabulary of [",
-                            paste0 (voc, collapse = ", "),
-                            "]"
-                        )
-                    )
-                }
-            }
+
+            out <- c (out, check_fs_meta_array (fs_terms, i, term_i))
+
         } else if (nzchar (fs_terms$vocabulary [i])) {
-            voc <- strsplit (fs_terms$vocabulary [i], "\\|") [[1]]
-            term_names <- c (
-                names (term_i),
-                unlist (lapply (term_i, names))
+
+            out <- c (out, check_fs_meta_from_vocab (fs_terms, i, term_i))
+        }
+    }
+
+    return (out)
+}
+
+#' Check one integer-valued figshare metadata term
+#' @noRd
+check_fs_meta_integer <- function (fs_terms, i, term_i) {
+
+    out <- NULL
+
+    if (is.na (suppressWarnings (as.integer (term_i)))) {
+
+        out <- paste0 (
+            "Data [",
+            fs_terms$term [i],
+            "] is not coercible to integer."
+        )
+    }
+
+    return (out)
+}
+
+#' Check one figshare metadata term against vocabulary entry
+#' @noRd
+check_fs_meta_from_vocab <- function (fs_terms, i, term_i) {
+
+    out <- NULL
+
+    voc <- strsplit (fs_terms$vocabulary [i], "\\|") [[1]]
+    term_names <- c (
+        names (term_i),
+        unlist (lapply (term_i, names))
+    )
+
+    if (!all (term_names %in% voc)) {
+        out <- paste0 (
+            "Data [",
+            fs_terms$term [i],
+            " = '",
+            term_i,
+            "' must follow fixed vocabulary of [",
+            paste0 (voc, collapse = ", "),
+            "]"
+        )
+    }
+
+    return (out)
+}
+
+#' Check one figshare metadata array term
+#' @noRd
+check_fs_meta_array <- function (fs_terms, i, term_i) {
+
+    out <- NULL
+
+    if (!is.list (term_i)) {
+
+        out <- paste0 (
+            "Data [",
+            fs_terms$term [i],
+            "] must have format [",
+            fs_terms$format [i],
+            "]"
+        )
+
+    } else if (nzchar (fs_terms$vocabulary [i])) {
+
+        voc <- strsplit (fs_terms$vocabulary [i], "\\|") [[1]]
+        term_names <- c (
+            names (term_i),
+            unlist (lapply (term_i, names))
+        )
+
+        if (!all (term_names %in% voc)) {
+
+            out <- paste0 (
+                "Data [",
+                fs_terms$term [i],
+                " = '",
+                term_i,
+                "' must follow fixed vocabulary of [",
+                paste0 (voc, collapse = ", "),
+                "]"
             )
-            if (!all (term_names %in% voc)) {
-                out <- c (
-                    out,
-                    paste0 (
-                        "Data [",
-                        fs_terms$term [i],
-                        " = '",
-                        term_i,
-                        "' must follow fixed vocabulary of [",
-                        paste0 (voc, collapse = ", "),
-                        "]"
-                    )
-                )
-            }
         }
     }
 
