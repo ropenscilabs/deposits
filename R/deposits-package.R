6762cb9451cdd66a31a7ca657b694cee41514262
mpadge <mark.padgham@email.com>
2022-05-19 08:48:45 +0200

    add coupla explicit function imports
    The are inside R6 functions so not found by rcmdcheck

--- R/deposits-package.R
+++ R/deposits-package.R
@@ -1,4 +1,7 @@
 #' @keywords internal
+#' @importFrom R6 R6Class
+#' @importFrom here here
+#' @importFrom curl curl_download
 "_PACKAGE"
 
 # The following block is used by usethis to automatically manage
