df4774af580790ba65f966487ada3f1935195daf
mpadge <mark.padgham@email.com>
2022-06-30 13:33:31 +0200

    update httptest2 mock results

--- tests/testthat/fs_new/api/account/articles-d27661-POST.R
+++ tests/testthat/fs_new/api/account/articles-d27661-POST.R
@@ -1,8 +1,8 @@
 structure(list(method = "POST", url = "api/account/articles", 
-    status_code = 201L, headers = structure(list(date = "Tue, 31 May 2022 09:00:27 GMT", 
+    status_code = 201L, headers = structure(list(date = "Thu, 30 Jun 2022 11:23:39 GMT", 
         `content-type` = "application/json", `content-length` = "108", 
-        location = "api/articles/19933529", server = "nginx", 
+        location = "api/articles/20198519", server = "nginx", 
         `access-control-allow-methods` = "GET, POST, PUT, DELETE, OPTIONS", 
         `access-control-allow-headers` = "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization", 
         `access-control-max-age` = "3600"), class = "httr2_headers"), 
-    body = charToRaw("{\"entity_id\": 19933529, \"location\": \"api/articles/19933529\", \"warnings\": []}")), class = "httr2_response")
+    body = charToRaw("{\"entity_id\": 20198519, \"location\": \"api/articles/20198519\", \"warnings\": []}")), class = "httr2_response")
