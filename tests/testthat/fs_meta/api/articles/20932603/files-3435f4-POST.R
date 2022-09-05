bdfcf973ccd54be0910169371938a2bf7b6eb2bf
mpadge <mark.padgham@email.com>
2022-09-05 10:32:12 +0200

    update more figshare-client test snapshots

--- tests/testthat/fs_meta/api/articles/20932603/files-3435f4-POST.R
+++ tests/testthat/fs_meta/api/articles/20932603/files-3435f4-POST.R
@@ -1,8 +1,12 @@
-structure(list(method = "POST", url = "api/articles/20932603/files", 
-    status_code = 201L, headers = structure(list(date = "Mon, 05 Sep 2022 08:31:04 GMT", 
-        `content-type` = "application/json", `content-length` = "84", 
-        location = "api/articles/20932603/files/37210261", server = "nginx", 
-        `access-control-allow-methods` = "GET, POST, PUT, DELETE, OPTIONS", 
-        `access-control-allow-headers` = "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization", 
-        `access-control-max-age` = "3600"), class = "httr2_headers"), 
-    body = charToRaw("{\"location\": \"api/articles/20932603/files/37210261\"}")), class = "httr2_response")
+structure (list (
+    method = "POST", url = "api/articles/20932603/files",
+    status_code = 201L, headers = structure (list (
+        date = "Mon, 05 Sep 2022 08:31:04 GMT",
+        `content-type` = "application/json", `content-length` = "84",
+        location = "api/articles/20932603/files/37210261", server = "nginx",
+        `access-control-allow-methods` = "GET, POST, PUT, DELETE, OPTIONS",
+        `access-control-allow-headers` = "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization",
+        `access-control-max-age` = "3600"
+    ), class = "httr2_headers"),
+    body = charToRaw ("{\"location\": \"api/articles/20932603/files/37210261\"}")
+), class = "httr2_response")
--- tests/testthat/fs_new/api/account/articles-d27661-POST.R
+++ tests/testthat/fs_new/api/account/articles-d27661-POST.R
@@ -1,8 +1,12 @@
-structure(list(method = "POST", url = "api/account/articles", 
-    status_code = 201L, headers = structure(list(date = "Mon, 05 Sep 2022 08:31:02 GMT", 
-        `content-type` = "application/json", `content-length` = "108", 
-        location = "api/articles/20932603", server = "nginx", 
-        `access-control-allow-methods` = "GET, POST, PUT, DELETE, OPTIONS", 
-        `access-control-allow-headers` = "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization", 
-        `access-control-max-age` = "3600"), class = "httr2_headers"), 
-    body = charToRaw("{\"entity_id\": 20932603, \"location\": \"api/articles/20932603\", \"warnings\": []}")), class = "httr2_response")
+structure (list (
+    method = "POST", url = "api/account/articles",
+    status_code = 201L, headers = structure (list (
+        date = "Mon, 05 Sep 2022 08:31:02 GMT",
+        `content-type` = "application/json", `content-length` = "108",
+        location = "api/articles/20932603", server = "nginx",
+        `access-control-allow-methods` = "GET, POST, PUT, DELETE, OPTIONS",
+        `access-control-allow-headers` = "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization",
+        `access-control-max-age` = "3600"
+    ), class = "httr2_headers"),
+    body = charToRaw ("{\"entity_id\": 20932603, \"location\": \"api/articles/20932603\", \"warnings\": []}")
+), class = "httr2_response")
