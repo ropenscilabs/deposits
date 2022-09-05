e414428d0c556662eb69f275fc83f842ea07ed95
mpadge <mark.padgham@email.com>
2022-09-05 10:31:57 +0200

    update figshare-client test snapshots

--- tests/testthat/fs_new/api/account/articles-d27661-POST.R
+++ tests/testthat/fs_new/api/account/articles-d27661-POST.R
@@ -1,8 +1,8 @@
 structure(list(method = "POST", url = "api/account/articles", 
-    status_code = 201L, headers = structure(list(date = "Thu, 30 Jun 2022 11:23:39 GMT", 
+    status_code = 201L, headers = structure(list(date = "Mon, 05 Sep 2022 08:31:02 GMT", 
         `content-type` = "application/json", `content-length` = "108", 
-        location = "api/articles/20198519", server = "nginx", 
+        location = "api/articles/20932603", server = "nginx", 
         `access-control-allow-methods` = "GET, POST, PUT, DELETE, OPTIONS", 
         `access-control-allow-headers` = "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization", 
         `access-control-max-age` = "3600"), class = "httr2_headers"), 
-    body = charToRaw("{\"entity_id\": 20198519, \"location\": \"api/articles/20198519\", \"warnings\": []}")), class = "httr2_response")
+    body = charToRaw("{\"entity_id\": 20932603, \"location\": \"api/articles/20932603\", \"warnings\": []}")), class = "httr2_response")
--- tests/testthat/fs_update/api/articles/20198519-84046d-PUT.R
+++
@@ -1,8 +0,0 @@
-structure(list(method = "PUT", url = "api/articles/20198519", 
-    status_code = 205L, headers = structure(list(date = "Thu, 30 Jun 2022 11:23:50 GMT", 
-        `content-type` = "application/json", `content-length` = "85", 
-        location = "api/articles/20198519", server = "nginx", 
-        `access-control-allow-methods` = "GET, POST, PUT, DELETE, OPTIONS", 
-        `access-control-allow-headers` = "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization", 
-        `access-control-max-age` = "3600"), class = "httr2_headers"), 
-    body = charToRaw("{\"location\": \"api/articles/20198519\", \"warnings\": []}")), class = "httr2_response")
