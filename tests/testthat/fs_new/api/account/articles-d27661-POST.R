c1a4cc68b1c52b576c66db18309d99eaec2a7ad2
mpadge <mark.padgham@email.com>
2022-10-06 13:16:52 +0200

    update httptest2 mock results

--- tests/testthat/fs_new/api/account/articles-d27661-POST.R
+++ tests/testthat/fs_new/api/account/articles-d27661-POST.R
@@ -1,12 +1,8 @@
-structure (list (
-    method = "POST", url = "api/account/articles",
-    status_code = 201L, headers = structure (list (
-        date = "Mon, 05 Sep 2022 08:31:02 GMT",
-        `content-type` = "application/json", `content-length` = "108",
-        location = "api/articles/20932603", server = "nginx",
-        `access-control-allow-methods` = "GET, POST, PUT, DELETE, OPTIONS",
-        `access-control-allow-headers` = "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization",
-        `access-control-max-age` = "3600"
-    ), class = "httr2_headers"),
-    body = charToRaw ("{\"entity_id\": 20932603, \"location\": \"api/articles/20932603\", \"warnings\": []}")
-), class = "httr2_response")
+structure(list(method = "POST", url = "api/account/articles", 
+    status_code = 201L, headers = structure(list(date = "Thu, 06 Oct 2022 11:15:02 GMT", 
+        `content-type` = "application/json", `content-length` = "108", 
+        location = "api/articles/21286683", server = "nginx", 
+        `access-control-allow-methods` = "GET, POST, PUT, DELETE, OPTIONS", 
+        `access-control-allow-headers` = "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization", 
+        `access-control-max-age` = "3600"), class = "httr2_headers"), 
+    body = charToRaw("{\"entity_id\": 21286683, \"location\": \"api/articles/21286683\", \"warnings\": []}")), class = "httr2_response")
