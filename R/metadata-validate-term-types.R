dbec7c9ce163ea152257412bcfbcaa12ab834245
mpadge <mark.padgham@email.com>
2022-10-20 17:33:40 +0200

    standardise param names in all validate-term-types fns for #8

--- R/metadata-validate-term-types.R
+++ R/metadata-validate-term-types.R
@@ -51,18 +51,18 @@ meta_validate_term_from_vocab <- function (these_terms, i, term_i) {
 #' Check one metadata langauge entry against ISO 639-2/3 values
 #' @noRd
 
-meta_validate_language_iso639 <- function (these_meta_terms, metaterms, i) {
+meta_validate_language_iso639 <- function (these_terms, metaterms, i) {
 
     out <- NULL
 
-    term_i <- metaterms [[these_meta_terms$term [i]]]
+    term_i <- metaterms [[these_terms$term [i]]]
 
     if (!term_i %in% iso_639_2_language_codes () [, 1]) {
         out <- paste0 (
             "Metadata [",
-            these_meta_terms$term [i],
+            these_terms$term [i],
             " = '",
-            metaterms [[these_meta_terms$term [i]]],
+            metaterms [[these_terms$term [i]]],
             "'] must be a three-letter ISO-639-2 or ISO-639-3 language identifier."
         )
     }
@@ -72,16 +72,16 @@ meta_validate_language_iso639 <- function (these_meta_terms, metaterms, i) {
 
 #' Check one metadata array term
 #' @noRd
-meta_validate_term_array <- function (these_meta_terms, metaterms, i) {
+meta_validate_term_array <- function (these_terms, metaterms, i) {
 
     out <- NULL
 
-    term_i <- metaterms [[these_meta_terms$term [i]]]
+    term_i <- metaterms [[these_terms$term [i]]]
 
     if (!is.list (term_i)) {
         out <- paste0 (
             "Metadata [",
-            these_meta_terms$term [i],
+            these_terms$term [i],
             "] must be an array/list object"
         )
     }
