f254871f74f18efae0775bc4d1f63ca437ca8318
mpadge <mark.padgham@email.com>
2022-12-08 15:07:18 +0100

    update httptest2 mock results for #28

--- tests/testthat/fs_update/api/articles/21533469/files-821717-POST.R
+++
@@ -1,9 +0,0 @@
-structure(list(method = "POST", url = "api/articles/21533469/files", 
-    status_code = 201L, headers = structure(list(date = "Thu, 10 Nov 2022 11:15:24 GMT", 
-        `content-type` = "application/json", `content-length` = "84", 
-        location = "api/articles/21533469/files/38169000", server = "nginx", 
-        `access-control-allow-origin` = "https://api.figshare.com", 
-        `access-control-allow-methods` = "GET, POST, PUT, DELETE, OPTIONS", 
-        `access-control-allow-headers` = "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization", 
-        `access-control-max-age` = "3600"), class = "httr2_headers"), 
-    body = charToRaw("{\"location\": \"api/articles/21533469/files/38169000\"}")), class = "httr2_response")
