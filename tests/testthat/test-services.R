dcc87cf4f9d7d8d89eb9b478668bb249a54bc804
mpadge <mark.padgham@email.com>
2022-12-20 16:53:00 +0100

    test 'deposit_service' method

--- tests/testthat/test-services.R
+++ tests/testthat/test-services.R
@@ -1,12 +1,12 @@
 
 test_all <- (identical (Sys.getenv ("MPADGE_LOCAL"), "true") |
-             identical (Sys.getenv ("GITHUB_WORKFLOW"), "test-coverage"))
+    identical (Sys.getenv ("GITHUB_WORKFLOW"), "test-coverage"))
 
-test_that("services", {
+test_that ("services", {
 
     expect_silent (
         s <- deposits_services ()
-        )
+    )
     expect_s3_class (s, "data.frame")
     expect_equal (ncol (s), 3L)
     expect_equal (nrow (s), 3L)
@@ -16,10 +16,14 @@ test_that("services", {
 
 test_that ("tokens", {
 
-    expect_error (get_deposits_token (),
-                  "Assertion on 'service' failed")
-    expect_error (get_deposits_token ("aaaaa"),
-                  "No token found for \\[aaaaa\\]")
+    expect_error (
+        get_deposits_token (),
+        "Assertion on 'service' failed"
+    )
+    expect_error (
+        get_deposits_token ("aaaaa"),
+        "No token found for \\[aaaaa\\]"
+    )
 
     testthat::skip_if (!test_all)
 
@@ -31,12 +35,25 @@ test_that ("tokens", {
     Sys.setenv ("junktwo" = "123456")
     expect_silent (
         tok <- get_deposits_token ("junk")
-        )
+    )
     expect_equal (tok, "123456")
 
     Sys.setenv ("junktwo" = "234567")
     expect_error (
         tok <- get_deposits_token ("junk"),
         "No unambiguous token found for \\[junk\\] service."
-        )
+    )
+})
+
+test_that ("deposit_service function", {
+
+    cli <- depositsClient$new (service = "zenodo", sandbox = TRUE)
+    expect_equal (cli$service, "zenodo")
+    expect_true (cli$sandbox)
+    expect_true (grepl ("zenodo", cli$url_base))
+
+    cli <- cli$deposit_service (service = "figshare")
+    expect_equal (cli$service, "figshare")
+    expect_false (cli$sandbox)
+    expect_true (grepl ("figshare", cli$url_base))
 })
