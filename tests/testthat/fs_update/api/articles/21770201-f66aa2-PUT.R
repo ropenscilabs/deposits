8da35b6497e3ea2eddd71ef0a194d4e30a4ea54c
mpadge <mark.padgham@email.com>
2022-12-22 14:58:03 +0100

    update httptest2 mock results for #28

--- tests/testthat/fs_update/api/articles/21770201-f66aa2-PUT.R
+++ tests/testthat/fs_update/api/articles/21770201-f66aa2-PUT.R
@@ -1,9 +1,9 @@
-structure(list(method = "PUT", url = "api/articles/21758093", 
-    status_code = 205L, headers = structure(list(date = "Tue, 20 Dec 2022 15:12:05 GMT", 
+structure(list(method = "PUT", url = "api/articles/21770201", 
+    status_code = 205L, headers = structure(list(date = "Thu, 22 Dec 2022 13:57:09 GMT", 
         `content-type` = "application/json", `content-length` = "85", 
-        location = "api/articles/21758093", server = "nginx", 
+        location = "api/articles/21770201", server = "nginx", 
         `access-control-allow-origin` = "https://api.figshare.com", 
         `access-control-allow-methods` = "GET, POST, PUT, DELETE, OPTIONS", 
         `access-control-allow-headers` = "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization", 
         `access-control-max-age` = "3600"), class = "httr2_headers"), 
-    body = charToRaw("{\"location\": \"api/articles/21758093\", \"warnings\": []}")), class = "httr2_response")
+    body = charToRaw("{\"location\": \"api/articles/21770201\", \"warnings\": []}")), class = "httr2_response")
