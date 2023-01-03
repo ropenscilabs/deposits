30e241a86f15fdb9d3cd24b97b000df98b642402
mpadge <mark.padgham@email.com>
2023-01-03 18:48:34 +0100

    update mock test results for #35

--- tests/testthat/fs_up/api/articles/21807069/files-1e2aa6-POST.R
+++ tests/testthat/fs_up/api/articles/21807069/files-1e2aa6-POST.R
@@ -1,9 +1,9 @@
-structure(list(method = "POST", url = "api/articles/21805392/files", 
-    status_code = 201L, headers = structure(list(date = "Tue, 03 Jan 2023 09:45:19 GMT", 
+structure(list(method = "POST", url = "api/articles/21807069/files", 
+    status_code = 201L, headers = structure(list(date = "Tue, 03 Jan 2023 17:47:25 GMT", 
         `content-type` = "application/json", `content-length` = "84", 
-        location = "api/articles/21805392/files/38693514", server = "nginx", 
+        location = "api/articles/21807069/files/38696094", server = "nginx", 
         `access-control-allow-origin` = "https://api.figshare.com", 
         `access-control-allow-methods` = "GET, POST, PUT, DELETE, OPTIONS", 
         `access-control-allow-headers` = "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization", 
         `access-control-max-age` = "3600"), class = "httr2_headers"), 
-    body = charToRaw("{\"location\": \"api/articles/21805392/files/38693514\"}")), class = "httr2_response")
+    body = charToRaw("{\"location\": \"api/articles/21807069/files/38696094\"}")), class = "httr2_response")
--- tests/testthat/fs_update/api/articles/21807069-2c9c1e-PUT.R
+++ tests/testthat/fs_update/api/articles/21807069-2c9c1e-PUT.R
@@ -1,9 +1,9 @@
-structure(list(method = "PUT", url = "api/articles/21805392", 
-    status_code = 205L, headers = structure(list(date = "Tue, 03 Jan 2023 09:45:19 GMT", 
+structure(list(method = "PUT", url = "api/articles/21807069", 
+    status_code = 205L, headers = structure(list(date = "Tue, 03 Jan 2023 17:47:24 GMT", 
         `content-type` = "application/json", `content-length` = "85", 
-        location = "api/articles/21805392", server = "nginx", 
+        location = "api/articles/21807069", server = "nginx", 
         `access-control-allow-origin` = "https://api.figshare.com", 
         `access-control-allow-methods` = "GET, POST, PUT, DELETE, OPTIONS", 
         `access-control-allow-headers` = "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization", 
         `access-control-max-age` = "3600"), class = "httr2_headers"), 
-    body = charToRaw("{\"location\": \"api/articles/21805392\", \"warnings\": []}")), class = "httr2_response")
+    body = charToRaw("{\"location\": \"api/articles/21807069\", \"warnings\": []}")), class = "httr2_response")
