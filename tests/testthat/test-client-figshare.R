ffdecfe422e09ccd81fe4779ecd6cefef831a809
mpadge <mark.padgham@email.com>
2023-01-31 15:51:07 +0100

    improve structure of test-client-figshare

--- tests/testthat/test-client-figshare.R
+++ tests/testthat/test-client-figshare.R
@@ -1,4 +1,3 @@
-
 test_all <- (identical (Sys.getenv ("MPADGE_LOCAL"), "true") |
     identical (Sys.getenv ("GITHUB_WORKFLOW"), "test-coverage"))
 
@@ -8,7 +7,7 @@ testthat::skip_if (!test_all)
 # standardised form (uniform timestamps and article id values).
 Sys.setenv ("DEPOSITS_TEST_ENV" = "true")
 
-test_that ("figshare actions", {
+test_that ("figshare new", {
 
     service <- "figshare"
 
@@ -52,15 +51,49 @@ test_that ("figshare actions", {
     expect_false (is.null (cli$hostdata))
     expect_type (cli$hostdata, "list")
     expect_true (length (cli$hostdata) > 1L)
+})
 
-    # -------- DEPOSIT_RETRIEVE
+new_mock_fs_deposit <- function () {
+
+    service <- "figshare"
+
+    cli <- with_mock_dir ("fs_create", {
+        depositsClient$new (service = service)
+    })
+    metadata <- list (
+        title = "New Title",
+        abstract = "This is the abstract",
+        creator = list ("A. Person", "B. Person")
+    )
+    cli <- with_mock_dir ("fs_client", {
+        depositsClient$new (
+            service = service,
+            metadata = metadata
+        )
+    })
+    cli <- with_mock_dir ("fs_new", {
+        cli$deposit_new ()
+    })
+
+    return (cli)
+}
+
+test_that ("figshare retrieve", {
+
+    service <- "figshare"
+    cli <- new_mock_fs_deposit ()
     deposit_id <- cli$id
+    metadata <- list (
+        title = "New Title",
+        abstract = "This is the abstract",
+        creator = list ("A. Person", "B. Person")
+    )
+
     dep <- with_mock_dir ("fs_retr", {
         cli$deposit_retrieve (deposit_id)
     })
     expect_s3_class (dep, "depositsClient")
 
-    # -------- DEPOSIT_UPDATE
     expect_equal (
         cli$hostdata$title,
         metadata$title
@@ -73,6 +106,14 @@ test_that ("figshare actions", {
         cli$metadata$title,
         metadata$title
     )
+
+})
+
+test_that ("figshare update", {
+
+    service <- "figshare"
+    cli <- new_mock_fs_deposit ()
+    deposit_id <- cli$id
 
     metadata <- list (
         title = "Modified Title",
@@ -103,8 +144,15 @@ test_that ("figshare actions", {
     #     cli$hostdata$description,
     #     metadata$abstract
     # )
+})
 
-    # --------- UPLOAD_DATA
+
+test_that ("figshare upload", {
+
+    service <- "figshare"
+    cli <- new_mock_fs_deposit ()
+    deposit_id <- cli$id
+
     # filename <- file.path (tempdir (), "data.Rds")
     # saveRDS (datasets::Orange, filename)
     filename <- fs::path (fs::path_temp (), "data.csv")
@@ -120,20 +168,29 @@ test_that ("figshare actions", {
         dep$files$supplied_md5,
         dep$files$computed_md5
     )
+})
 
-    # -------- DEPOSITS_LIST
+test_that ("figshare list", {
+
+    cli <- new_mock_fs_deposit ()
+
     dep <- with_mock_dir ("fs_list", {
         cli$deposits_list ()
     })
 
     expect_s3_class (dep, "depositsClient")
     expect_identical (dep, cli)
+})
 
-    # -------- DEPOSIT_DOWNLOAD
+
+test_that ("figshare download", {
+
     # can't test because figshare doesn't allow private download
     # ---> tested in zenodo.
+})
 
-    # -------- DEPOSIT_DELETE
+test_that ("figshare delete", {
+
     # can't mock that because it returns an empty body
     # dep <- with_mock_dir ("fs_del", {
     #     cli$deposit_delete (deposit_id)
