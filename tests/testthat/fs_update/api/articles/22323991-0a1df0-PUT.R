8df7dbe3016acad2350a01aa6c3a2e29c21c3d04
mpadge <mark.padgham@email.com>
2023-03-23 12:06:26 +0100

    update mock test results with mapping of single json source -> multiple targets for #39

--- tests/testthat/fs_update/api/articles/22323991-0a1df0-PUT.R
+++ tests/testthat/fs_update/api/articles/22323991-0a1df0-PUT.R
@@ -1,9 +1,9 @@
-structure(list(method = "PUT", url = "api/articles/22323748", 
-    status_code = 205L, headers = structure(list(date = "Thu, 23 Mar 2023 10:22:49 GMT", 
+structure(list(method = "PUT", url = "api/articles/22323991", 
+    status_code = 205L, headers = structure(list(date = "Thu, 23 Mar 2023 11:00:52 GMT", 
         `content-type` = "application/json", `content-length` = "85", 
-        location = "api/articles/22323748", server = "nginx", 
+        location = "api/articles/22323991", server = "nginx", 
         `access-control-allow-origin` = "https://api.figshare.com", 
         `access-control-allow-methods` = "GET, POST, PUT, DELETE, OPTIONS", 
         `access-control-allow-headers` = "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization", 
         `access-control-max-age` = "3600"), class = "httr2_headers"), 
-    body = charToRaw("{\"location\": \"api/articles/22323748\", \"warnings\": []}")), class = "httr2_response")
+    body = charToRaw("{\"location\": \"api/articles/22323991\", \"warnings\": []}")), class = "httr2_response")
