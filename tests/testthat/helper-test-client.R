f7641d1f2e0754f0647707812b6f8f4613fa4f0d
mpadge <mark.padgham@email.com>
2023-03-28 15:37:44 +0200

    fix testthat/helper-test-client.R for #36

--- tests/testthat/helper-test-client.R
+++ tests/testthat/helper-test-client.R
@@ -5,22 +5,21 @@ new_mock_deposit <- function (service = "zenodo") {
         title = "New Title",
         abstract = "This is the abstract",
         creator = list (list (name = "A. Person"), list (name = "B. Person")),
-        description = paste0 (
-            "## description\nThis is the description\n\n",
-            "## keywords\none, two\nthree\n\n## version\n1.0"
-        )
+        description =
+            "## description\nThis is the description\n\n## version\n1.0",
+        subject = "## keywords\none, two\nthree"
     )
 
     prfx <- ifelse (service == "zenodo", "zen", "fs")
 
-    cli <- with_mock_dir (paste0 (prfx, "_client"), {
+    cli <- httptest2::with_mock_dir (paste0 (prfx, "_client"), {
         depositsClient$new (
             service = service,
             sandbox = TRUE,
             metadata = metadata
         )
     })
-    cli <- with_mock_dir (paste0 (prfx, "_new"), {
+    cli <- httptest2::with_mock_dir (paste0 (prfx, "_new"), {
         cli$deposit_new ()
     })
 
