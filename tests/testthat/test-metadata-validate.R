43c3326dd2ae96c6f5462bfdc75dd79f967476ec
mpadge <mark.padgham@email.com>
2023-03-28 11:34:48 +0200

    add test for auto-remove unrecognised metadata properties for #57

--- tests/testthat/test-metadata-validate.R
+++ tests/testthat/test-metadata-validate.R
@@ -11,8 +11,21 @@ test_that ("metadata validate", {
         description = paste0 (
             "## description\nThis is the description\n\n",
             "## keywords\none, two\nthree\n\n## version\n1.0"
-        )
+        ),
+        not = "Not a property"
     )
+
+    expect_warning (
+        metadata_valid <- validate_metadata (metadata, service = "zenodo"),
+        "The following metadata terms do not conform and will be removed"
+    )
+    nms_in <- sort (names (metadata))
+    nms_out <- sort (names (metadata_valid$dcmi))
+    # 'not' has been removed:
+    expect_equal (length (nms_in) - 1L, length (nms_out))
+    expect_identical (nms_in [which (!nms_in == "not")], nms_out)
+    # Then remove for further tests:
+    metadata$not <- NULL
 
     # Zenodo allows creators to have 'name', 'affiliation', 'orcid', 'gnd'
     # Figshare allows creators to have 'name', 'id', 'first_name', 'last_name',
