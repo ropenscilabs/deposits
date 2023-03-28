e386c6c640c0dacb5a928e7b1710593703dc352e
mpadge <mark.padgham@email.com>
2023-03-28 11:18:13 +0200

    add 'test-metadata-validate.R'; closes #57

---
+++ tests/testthat/test-metadata-validate.R
@@ -0,0 +1,50 @@
+test_that ("metadata validate", {
+
+    ORCID <- "0000-0000-0000-0000" # nolint
+    metadata <- list (
+        title = "New Title",
+        abstract = "This is the abstract",
+        creator = list (
+            list (name = "A. Person", affiliation = "no", orcid = ORCID),
+            list (name = "B. Person")
+        ),
+        description = paste0 (
+            "## description\nThis is the description\n\n",
+            "## keywords\none, two\nthree\n\n## version\n1.0"
+        )
+    )
+
+    # Zenodo allows creators to have 'name', 'affiliation', 'orcid', 'gnd'
+    # Figshare allows creators to have 'name', 'id', 'first_name', 'last_name',
+    # 'email', 'orcid'
+
+    expect_silent (
+        metadata_valid <- validate_metadata (metadata, service = "zenodo")
+    )
+    expect_error (
+        metadata_valid <- validate_metadata (metadata, service = "figshare"),
+        "Stopping because the metadata terms listed above do not conform"
+    )
+
+    metadata$creator <- list (
+        list (name = "A. Person", id = "no", orcid = ORCID), # id must be integer
+        list (name = "B. Person")
+    )
+    expect_error (
+        metadata_valid <- validate_metadata (metadata, service = "figshare"),
+        "Stopping because the DCMI metadata terms listed above do not conform"
+    )
+
+    # Numeric in R still passes as integer in JSON:
+    metadata$creator [[1]]$id <- 1
+    expect_silent (
+        metadata_valid <- validate_metadata (metadata, service = "figshare")
+    )
+
+    # But fails if passed any decimals:
+    metadata$creator [[1]]$id <- 1.1
+    expect_error (
+        metadata_valid <- validate_metadata (metadata, service = "figshare"),
+        "Stopping because the DCMI metadata terms listed above do not conform"
+    )
+})
