000997b51476a4522f3a36370380a976530b2a9c
mpadge <mark.padgham@email.com>
2023-03-28 15:37:15 +0200

    update figshare httptest2 mock results for #36

--- tests/testthat/fs_up2/api/articles/22348423/files-e3c461-POST.R
+++ tests/testthat/fs_up2/api/articles/22348423/files-e3c461-POST.R
@@ -1,9 +1,9 @@
-structure(list(method = "POST", url = "api/articles/22323991/files", 
-    status_code = 201L, headers = structure(list(date = "Thu, 23 Mar 2023 11:00:54 GMT", 
+structure(list(method = "POST", url = "api/articles/22348423/files", 
+    status_code = 201L, headers = structure(list(date = "Tue, 28 Mar 2023 13:34:23 GMT", 
         `content-type` = "application/json", `content-length` = "84", 
-        location = "api/articles/22323991/files/39714577", server = "nginx", 
+        location = "api/articles/22348423/files/39762793", server = "nginx", 
         `access-control-allow-origin` = "https://api.figshare.com", 
         `access-control-allow-methods` = "GET, POST, PUT, DELETE, OPTIONS", 
         `access-control-allow-headers` = "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization", 
         `access-control-max-age` = "3600"), class = "httr2_headers"), 
-    body = charToRaw("{\"location\": \"api/articles/22323991/files/39714577\"}")), class = "httr2_response")
+    body = charToRaw("{\"location\": \"api/articles/22348423/files/39762793\"}")), class = "httr2_response")
