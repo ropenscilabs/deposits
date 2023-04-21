0445c5c5e5070d5419a9a45a6f28218190ae0b9e
mpadge <mark.padgham@email.com>
2023-04-21 14:43:09 +0200

    namespace tar and zip

--- R/deposits-package.R
+++ R/deposits-package.R
@@ -1,6 +1,7 @@
 #' @keywords internal
 #' @importFrom R6 R6Class
 #' @importFrom here here
+#' @importFrom utils tar zip
 "_PACKAGE"
 
 # The following block is used by usethis to automatically manage
