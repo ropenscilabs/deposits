b3eeb1da4ba6e3615bc3a8acc2403f5d43fa8575
mpadge <mark.padgham@email.com>
2023-06-12 17:05:40 +0200

    update fs mock test results

---
+++ tests/testthat/fs_up2/api/articles/23502069/files/41219382-POST.R
@@ -0,0 +1,9 @@
+structure(list(method = "POST", url = "api/articles/23502069/files/41219382", 
+    status_code = 202L, headers = structure(list(date = "Mon, 12 Jun 2023 15:02:34 GMT", 
+        `content-type` = "text/html; charset=UTF-8", `content-length` = "157", 
+        location = "api/articles/23502069/files/41219382", server = "nginx", 
+        `access-control-allow-origin` = "https://api.figshare.com", 
+        `access-control-allow-methods` = "GET, POST, PUT, DELETE, OPTIONS", 
+        `access-control-allow-headers` = "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization", 
+        `access-control-max-age` = "3600"), class = "httr2_headers"), 
+    body = charToRaw("<html>\n <head>\n  <title>202 Accepted</title>\n </head>\n <body>\n  <h1>202 Accepted</h1>\n  The request is accepted for processing.<br/><br/>\n\n\n\n </body>\n</html>")), class = "httr2_response")
