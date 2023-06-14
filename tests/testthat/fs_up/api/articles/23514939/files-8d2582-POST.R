83f4942f975b1753855417c2200d0aee8c6a1217
mpadge <mark.padgham@email.com>
2023-06-14 13:35:09 +0200

    update fs mock test results again for #83

---
+++ tests/testthat/fs_up/api/articles/23514939/files-8d2582-POST.R
@@ -0,0 +1,9 @@
+structure(list(method = "POST", url = "api/articles/23514939/files", 
+    status_code = 201L, headers = structure(list(date = "Wed, 14 Jun 2023 11:33:35 GMT", 
+        `content-type` = "application/json", `content-length` = "84", 
+        location = "api/articles/23514939/files/41239023", server = "nginx", 
+        `access-control-allow-origin` = "https://api.figshare.com", 
+        `access-control-allow-methods` = "GET, POST, PUT, DELETE, OPTIONS", 
+        `access-control-allow-headers` = "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization", 
+        `access-control-max-age` = "3600"), class = "httr2_headers"), 
+    body = charToRaw("{\"location\": \"api/articles/23514939/files/41239023\"}")), class = "httr2_response")
--- tests/testthat/fs_up2/api/articles/23514939-54b0b5-PUT.R
+++ tests/testthat/fs_up2/api/articles/23514939-54b0b5-PUT.R
@@ -1,9 +1,9 @@
-structure(list(method = "POST", url = "api/account/articles", 
-    status_code = 201L, headers = structure(list(date = "Wed, 14 Jun 2023 11:33:29 GMT", 
-        `content-type` = "application/json", `content-length` = "108", 
+structure(list(method = "PUT", url = "api/articles/23514939", 
+    status_code = 205L, headers = structure(list(date = "Wed, 14 Jun 2023 11:33:57 GMT", 
+        `content-type` = "application/json", `content-length` = "85", 
         location = "api/articles/23514939", server = "nginx", 
         `access-control-allow-origin` = "https://api.figshare.com", 
         `access-control-allow-methods` = "GET, POST, PUT, DELETE, OPTIONS", 
         `access-control-allow-headers` = "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization", 
         `access-control-max-age` = "3600"), class = "httr2_headers"), 
-    body = charToRaw("{\"entity_id\": 23514939, \"location\": \"api/articles/23514939\", \"warnings\": []}")), class = "httr2_response")
+    body = charToRaw("{\"location\": \"api/articles/23514939\", \"warnings\": []}")), class = "httr2_response")
