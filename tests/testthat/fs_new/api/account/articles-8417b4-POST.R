b9048f852b8f29ac30e8eb5101dca0f8828119c6
mpadge <mark.padgham@email.com>
2023-06-14 13:24:29 +0200

    add coupla missing mock test files for #83

---
+++ tests/testthat/fs_new/api/account/articles-8417b4-POST.R
@@ -0,0 +1,9 @@
+structure(list(method = "POST", url = "api/account/articles", 
+    status_code = 201L, headers = structure(list(date = "Wed, 14 Jun 2023 10:57:01 GMT", 
+        `content-type` = "application/json", `content-length` = "108", 
+        location = "api/articles/23514888", server = "nginx", 
+        `access-control-allow-origin` = "https://api.figshare.com", 
+        `access-control-allow-methods` = "GET, POST, PUT, DELETE, OPTIONS", 
+        `access-control-allow-headers` = "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization", 
+        `access-control-max-age` = "3600"), class = "httr2_headers"), 
+    body = charToRaw("{\"entity_id\": 23514888, \"location\": \"api/articles/23514888\", \"warnings\": []}")), class = "httr2_response")
