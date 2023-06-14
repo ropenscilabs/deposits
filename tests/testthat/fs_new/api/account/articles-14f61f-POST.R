12a27a1a1f43eed6a0614797640382205bc5c84e
mpadge <mark.padgham@email.com>
2023-06-14 13:07:49 +0200

    update fs mock test results for #83

--- tests/testthat/fs_new/api/account/articles-14f61f-POST.R
+++
@@ -1,9 +0,0 @@
-structure(list(method = "POST", url = "api/account/articles", 
-    status_code = 201L, headers = structure(list(date = "Mon, 12 Jun 2023 15:02:26 GMT", 
-        `content-type` = "application/json", `content-length` = "108", 
-        location = "api/articles/23502069", server = "nginx", 
-        `access-control-allow-origin` = "https://api.figshare.com", 
-        `access-control-allow-methods` = "GET, POST, PUT, DELETE, OPTIONS", 
-        `access-control-allow-headers` = "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization", 
-        `access-control-max-age` = "3600"), class = "httr2_headers"), 
-    body = charToRaw("{\"entity_id\": 23502069, \"location\": \"api/articles/23502069\", \"warnings\": []}")), class = "httr2_response")
--- tests/testthat/fs_update_dp1/api/articles/23502069-f280f7-PUT.R
+++
@@ -1,9 +0,0 @@
-structure(list(method = "PUT", url = "api/articles/23502069", 
-    status_code = 205L, headers = structure(list(date = "Mon, 12 Jun 2023 15:02:37 GMT", 
-        `content-type` = "application/json", `content-length` = "85", 
-        location = "api/articles/23502069", server = "nginx", 
-        `access-control-allow-origin` = "https://api.figshare.com", 
-        `access-control-allow-methods` = "GET, POST, PUT, DELETE, OPTIONS", 
-        `access-control-allow-headers` = "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization", 
-        `access-control-max-age` = "3600"), class = "httr2_headers"), 
-    body = charToRaw("{\"location\": \"api/articles/23502069\", \"warnings\": []}")), class = "httr2_response")
