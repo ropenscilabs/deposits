8b17cfe1a69f32b3a14f8497b03e4d5d81e95a65
mpadge <mark.padgham@email.com>
2023-06-15 11:28:26 +0200

    test figshare deposit_version error for #77

--- tests/testthat/fs_up2/api/articles/23522487/files-e3c461-POST.R
+++ tests/testthat/fs_up2/api/articles/23522487/files-e3c461-POST.R
@@ -1,9 +1,9 @@
-structure(list(method = "POST", url = "api/articles/23514939/files", 
-    status_code = 201L, headers = structure(list(date = "Wed, 14 Jun 2023 11:33:35 GMT", 
+structure(list(method = "POST", url = "api/articles/23522487/files", 
+    status_code = 201L, headers = structure(list(date = "Thu, 15 Jun 2023 09:26:27 GMT", 
         `content-type` = "application/json", `content-length` = "84", 
-        location = "api/articles/23514939/files/41239023", server = "nginx", 
+        location = "api/articles/23522487/files/41251092", server = "nginx", 
         `access-control-allow-origin` = "https://api.figshare.com", 
         `access-control-allow-methods` = "GET, POST, PUT, DELETE, OPTIONS", 
         `access-control-allow-headers` = "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization", 
         `access-control-max-age` = "3600"), class = "httr2_headers"), 
-    body = charToRaw("{\"location\": \"api/articles/23514939/files/41239023\"}")), class = "httr2_response")
+    body = charToRaw("{\"location\": \"api/articles/23522487/files/41251092\"}")), class = "httr2_response")
