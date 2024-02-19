99e56138d11910afefd611d61d5d0f857bae3665
mpadge <mark.padgham@email.com>
2024-02-19 12:38:55 +0100

    update zenodo licenses for #91

--- data-raw/get-zenodo-licenses.R
+++ data-raw/get-zenodo-licenses.R
@@ -1,11 +1,16 @@
 token <- Sys.getenv ("ZENODO_TOKEN")
-url <- "https://zenodo.org/api/licenses?size=1000"
+url <- "https://zenodo.org/api/licenses"
 headers <- list (Authorization = paste0 ("Bearer ", token))
-con <- crul::HttpClient$new (url, headers = headers)
-res <- con$get ()
-lic <- jsonlite::fromJSON (res$parse (encoding = "UTF-8"))
-lic <- lic$hits$hits$metadata
-lic <- lic [, c ("id", "title", "url")]
+req <- httr2::request (url)
+req <- httr2::req_method (req, "GET")
+req <- httr2::req_url_query (req, size = 1000)
+resp <- httr2::req_perform (req)
+resp_body <- httr2::resp_body_json (resp, simplify = TRUE)
+
+lic <- resp_body$hits$hits [, c ("id", "title", "links")]
+lic$title <- unlist (lic$title)
+lic$links <- unlist (lic$links)
+names (lic) [3] <- "url"
 
 path <- fs::path (here::here (), "inst", "extdata", "zenodo", "zenodo_licenses.csv")
 write.csv (lic, file = path, row.names = FALSE, fileEncoding = "UTF-8")
